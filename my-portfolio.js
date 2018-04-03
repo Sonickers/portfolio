8e6c850e04601bcf578e7d79929c957fb3376eb8
Sonickers <35639449+Sonickers@users.noreply.github.com>
2018-04-03 21:13:01 +0200

    x

--- my-portfolio.js
+++ my-portfolio.js
@@ -9,7 +9,11 @@
                     // Check if data is not older than half an hour, in ms
                     if (Date.now() - timestamp < 1800000) {
                         const images = JSON.parse(localStorage.getItem('insta-images'));
-                        displayImages(images);
+                        window.allImages = images;
+                        displayImages(images.slice(0,6));
+                        if (images.length > 6) {
+                            $('.js-show-more-btn').show();
+                        }
                         return false;
                     } else {
                         localStorage.clear();
@@ -19,16 +23,32 @@
             },
         }).done(function(res) {
             const images = res.graphql.user.edge_owner_to_timeline_media.edges.map((x) => x.node);
-            displayImages(images);
+            window.allImages = images;
+            displayImages(images.slice(0,6));
+
+            if (images.length > 6) {
+                $('.js-show-more-btn').show();
+            }
 
             if (localStorage) {
                 localStorage.setItem('insta-images', JSON.stringify(images));
                 localStorage.setItem('insta-images-timestamp', Date.now());
             }
         });
+
+        $(document).on('click', '.js-show-more-btn', function(e) {
+            e.preventDefault();
+
+            if (window.allImages !== undefined) {
+                displayImages(window.allImages);
+                $('.js-show-more-btn').remove();
+            }
+        });
     });
 
     function displayImages(images) {
+        $('.js-drawings-container').html('');
+
         images.forEach((image) => {
             const thumbnail = image.thumbnail_src;
             const text = image.edge_media_to_caption.edges[0].node.text;
