8c4682ec81a8f23cffdcae83386af8c8b6bafccc
Michal Repec <mikerepec@gmail.com>
2019-01-02 12:06:52 +0100

    Updated js to use new api endpoint

--- my-portfolio.js
+++ my-portfolio.js
@@ -1,28 +1,14 @@
 (($) => {
     $(function() {
         $.ajax({
-            url: '/insta.php',
+            url: 'https://sonia-portfolio-api.herokuapp.com/insta',
             context: $('.js-drawings-container'),
-            beforeSend: function() {
-                if (localStorage) {
-                    const timestamp = +localStorage.getItem('insta-images-timestamp');
-                    // Check if data is not older than half an hour, in ms
-                    if (Date.now() - timestamp < 1800000) {
-                        const images = JSON.parse(localStorage.getItem('insta-images'));
-                        window.allImages = images;
-                        displayImages(images.slice(0,6));
-                        if (images.length > 6) {
-                            $('.js-show-more-btn').show();
-                        }
-                        return false;
-                    } else {
-                        localStorage.clear();
-                    }
-                }
-                return true;
+            error: function() {
+                $('.js-drawings-container').html('Problem fetching from api...');
             },
+            timeout: 3000
         }).done(function(res) {
-            const images = res.media;
+            const images = res.posts;
             window.allImages = images;
             displayImages(images.slice(0,6));
 
@@ -49,16 +35,13 @@
     function displayImages(images) {
         $('.js-drawings-container').html('');
 
-        images.forEach((image) => {
-            const thumbnail = image.thumbnailSrc;
-            const text = image.caption;
-            const url = image.link;
+        images.forEach(({ caption, thumbnail_url, link }) => {
             const template = $('.js-drawing-template').clone();
 
             template.removeClass('js-drawing-template');
-            template.find('img').attr('src', thumbnail);
-            template.find('p').html(text);
-            template.find('a').attr('href', url);
+            template.find('img').attr('src', thumbnail_url);
+            template.find('p').html(caption);
+            template.find('a').attr('href', link);
 
             $('.js-drawings-container').append(template);
         });
